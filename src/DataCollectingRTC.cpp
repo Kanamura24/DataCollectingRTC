1e57ab4551484e3e3c7b0a4c5e51fc7a9771dea8
k <kanamura@kanamuranoMacBook-Pro.local>
2020-11-17 21:29:40 +0900

    add save image func.

--- src/DataCollectingRTC.cpp
+++ src/DataCollectingRTC.cpp
@@ -13,6 +13,17 @@
 
 #include "DataCollectingRTC.h"
 
+#include <iomanip>
+#include <ctime>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#ifdef WIN32
+#include <direct.h>
+#else
+#include <sys/stat.h>
+#endif
+
 // Module specification
 // <rtc-template block="module_spec">
 static const char* datacollectingrtc_spec[] =
@@ -122,18 +133,118 @@ RTC::ReturnCode_t DataCollectingRTC::onShutdown(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t DataCollectingRTC::onActivated(RTC::UniqueId ec_id)
 {
+
+  time_t now = std::time(NULL);
+ 	struct tm* localNow = std::localtime(&now);
+ 	std::ostringstream ss;
+ 	ss << "log"
+ 		<< 1900 + localNow->tm_year
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_mon + 1
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_mday
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_hour
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_min
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_sec;
+
+ 	m_logDir = ss.str();
+ #ifdef WIN32
+ 	_mkdir(m_logDir.c_str());
+ #else
+ 	mkdir(m_logDir.c_str(), 0777);
+ #endif
+
+  std::string filename = m_logDir + "/motion.csv";
+  m_JointLog.open(filename.c_str(), std::ios::out);
+
+	
   return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t DataCollectingRTC::onDeactivated(RTC::UniqueId ec_id)
 {
+  m_JointLog.close();
+  
   return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t DataCollectingRTC::onExecute(RTC::UniqueId ec_id)
 {
+   std::cout << "Waiting for CameraImage...." << std::ends;
+
+   time_t now = std::time(NULL);
+   struct tm* localNow = std::localtime(&now);
+   std::ostringstream ss;
+   ss 
+     << "image"
+     << 1900 + localNow->tm_year
+     << std::setw(2) << std::setfill('0') << localNow->tm_mon + 1
+     << std::setw(2) << std::setfill('0') << localNow->tm_mday
+     << std::setw(2) << std::setfill('0') << localNow->tm_hour
+     << std::setw(2) << std::setfill('0') << localNow->tm_min
+     << std::setw(2) << std::setfill('0') << localNow->tm_sec
+     << ".png";
+
+   coil::sleep(m_camera_wait_time);
+   
+   std::string filename = ss.str();
+
+
+ #if 1
+   /// Capture Image and Save
+   bool imageArrived = false;
+   //long counter = 0;
+
+  //Inport data check
+  while (m_cameraIn.isNew() && (!imageArrived)) {
+    m_cameraIn.read();
+    imageArrived = true;
+  }
+  std::cout << "[ArmImageGenerator] Image Arrived." << std::endl;
+  
+  long width = m_camera.data.image.width;
+  long height = m_camera.data.image.height;
+  long channels = (m_camera.data.image.format == Img::CF_GRAY) ? 1 :
+    (m_camera.data.image.format == Img::CF_RGB || m_camera.data.image.format == Img::CF_PNG || m_camera.data.image.format == Img::CF_JPEG) ? 3 :
+    (m_camera.data.image.raw_data.length() / width / height);
+  
+  if (channels == 3)
+    m_buffer.create(height, width, CV_8UC3);
+  else
+    m_buffer.create(height, width, CV_8UC1);
+  
+  long data_length = m_camera.data.image.raw_data.length();
+
+  //long image_size = width * height * channels;
+  
+  if (m_camera.data.image.format == Img::CF_RGB) {
+    for (int i = 0; i<height; ++i)
+      memcpy(&m_buffer.data[i*m_buffer.step], &m_camera.data.image.raw_data[i*width*channels], sizeof(unsigned char)*width*channels);
+    if (channels == 3)
+      cv::cvtColor(m_buffer, m_buffer, CV_RGB2BGR);
+  }
+  else if (m_camera.data.image.format == Img::CF_JPEG || m_camera.data.image.format == Img::CF_PNG) {
+    std::vector<uchar> compressed_image = std::vector<uchar>(data_length);
+    memcpy(&compressed_image[0], &m_camera.data.image.raw_data[0], sizeof(unsigned char) * data_length);
+    
+    //Decode received compressed image
+    cv::Mat decoded_image;
+    if (channels == 3) {
+      decoded_image = cv::imdecode(cv::Mat(compressed_image), CV_LOAD_IMAGE_COLOR);
+      cv::cvtColor(decoded_image, m_buffer, CV_RGB2BGR);
+    }
+    else {
+      decoded_image = cv::imdecode(cv::Mat(compressed_image), CV_LOAD_IMAGE_GRAYSCALE);
+      m_buffer = decoded_image;
+    }
+  }
+  
+  cv::imwrite(m_logDir + "/" + filename, m_buffer);
+  
+#endif  
+
+  m_JointLog << filename << std::endl;
+
   float sum = 0;
 
   if(m_inIn.isNew()) {
@@ -143,6 +254,9 @@ RTC::ReturnCode_t DataCollectingRTC::onExecute(RTC::UniqueId ec_id)
     std::cout << "Sum= " << sum << std::endl; 
   }
   coil::sleep(2.0);
+
+  m_JointLog.flush();
+  
   return RTC::RTC_OK;
 }
 
