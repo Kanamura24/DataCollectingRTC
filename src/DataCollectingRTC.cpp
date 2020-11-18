3918eb2e4c03fd9110e69f7d78464ee522b47ab1
k <kanamura@kanamuranoMacBook-Pro.local>
2020-11-18 20:23:05 +0900

    edit time sleep.

--- src/DataCollectingRTC.cpp
+++ src/DataCollectingRTC.cpp
@@ -194,7 +194,7 @@ RTC::ReturnCode_t DataCollectingRTC::onExecute(RTC::UniqueId ec_id)
      << std::setw(2) << std::setfill('0') << localNow->tm_sec
      << ".png";
 
-   coil::sleep(3.0);
+   coil::sleep(1.0);
    
    std::string filename = ss.str();
 
